f92db973ba6a8705a28a9ca5524191d1fc326ab5
mr-arty <keith.elam.guru@gmail.com>
2025-08-02 14:53:34 +0500

    Changed domain name to placeholder value

--- roles/install-technitium/files/pkcs12convert.sh
+++ roles/install-technitium/files/pkcs12convert.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-openssl pkcs12 -export -out /etc/letsencrypt/live/dns.frkn.org/dns.frkn.org.pfx -inkey /etc/letsencrypt/live/dns.frkn.org/privkey.pem -in /etc/letsencrypt/live/dns.frkn.org/cert.pem -certfile /etc/letsencrypt/live/dns.frkn.org/chain.pem -passout pass:
+openssl pkcs12 -export -out /etc/letsencrypt/live/dns.yourdomain.com/dns.yourdomain.com.pfx -inkey /etc/letsencrypt/live/dns.yourdomain.com/privkey.pem -in /etc/letsencrypt/live/dns.yourdomain.com/cert.pem -certfile /etc/letsencrypt/live/dns.yourdomain.com/chain.pem -passout pass:
 echo "pkcs#12 generated!"
