13af0212ebbec5bba08fd7833f110bf0da501d67
mr-arty <keith.elam.guru@gmail.com>
2025-08-09 20:13:30 +0500

    Added env variables

--- roles/install-technitium/files/pkcs12convert.sh
+++ roles/install-technitium/files/pkcs12convert.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-openssl pkcs12 -export -out /etc/letsencrypt/live/dns.frdm.app/dns.frdm.app.pfx -inkey /etc/letsencrypt/live/dns.frdm.app/key.pem -in /etc/letsencrypt/live/dns.frdm.app/cert.pem -certfile /etc/letsencrypt/live/dns.frdm.app/chain.pem -passout pass:
+HOSTNAME=$1
+
+openssl pkcs12 -export -out /etc/letsencrypt/live/$HOSTNAME/$HOSTNAME.pfx -inkey /etc/letsencrypt/live/$HOSTNAME/key.pem -in /etc/letsencrypt/live/$HOSTNAME/cert.pem -certfile /etc/letsencrypt/live/$HOSTNAME/chain.pem -passout pass:
 echo "pkcs#12 generated!"
