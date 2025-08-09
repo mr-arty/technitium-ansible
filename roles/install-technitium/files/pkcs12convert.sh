2fab678aff3e324c41528398db74a51f7a27c132
mr-arty <keith.elam.guru@gmail.com>
2025-08-09 18:05:16 +0500

    Updated install-technitium role, added acme-companion

--- roles/install-technitium/files/pkcs12convert.sh
+++ roles/install-technitium/files/pkcs12convert.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-openssl pkcs12 -export -out /etc/letsencrypt/live/dns.frkn.org/dns.frkn.org.pfx -inkey /etc/letsencrypt/live/dns.frkn.org/privkey.pem -in /etc/letsencrypt/live/dns.frkn.org/cert.pem -certfile /etc/letsencrypt/live/dns.frkn.org/chain.pem -passout pass:
+openssl pkcs12 -export -out /etc/letsencrypt/live/dns.frdm.app/dns.frdm.app.pfx -inkey /etc/letsencrypt/live/dns.frdm.app/key.pem -in /etc/letsencrypt/live/dns.frdm.app/cert.pem -certfile /etc/letsencrypt/live/dns.frdm.app/chain.pem -passout pass:
 echo "pkcs#12 generated!"
